a119802612aa97d4041220a60049f6fe741d1d38
asanka-j <asanka.jsndr@gmail.com>
2020-04-25 18:52:42 +0530

    updated post spider with df and yeild

--- spiders/postspider.py
+++ spiders/postspider.py
@@ -8,18 +8,50 @@ class PostSpider(scrapy.Spider):
         'https://blog.scrapinghub.com/page/2',
     ]
     
-
-    '''  parse the data with css  selectors  '''
-
+    '''  parse the data with with python generators  '''
     def parse(self, response):
+        '''
+        crawls the data : use following format to run the crawler 
+        scrapy crawl posts -o [filename].[filetype] 
+        '''
+    
+        for post in response.css('div.post-item'):
+            postheader = post.css('.post-header')
+            postbyline = postheader.css('.byline')
+
+            yield {
+                'postdate' : postbyline.css('.date a::text').get(),
+                'posttitle' : postheader.css('h2 a::text').get(),
+                'postauthor' : postbyline.css('.author a::text').get(),
+                'postContent' : post.css('.post-content p ::text').get(),
+                'postLink' : postheader.css('h2 a::attr(href)').get()
+            }
+
+
+
+class PostDFdSpider(scrapy.Spider):
+    name = "postDF"
+    start_urls= [
+        'https://blog.scrapinghub.com/page/1',
+        'https://blog.scrapinghub.com/page/2',
+    ]
+    
+    '''  parse the data with css  selectors and pd dataframes  '''
+    def parse(self, response):
+        '''
+        crawls the data,manupulate with pd dataframes and save as csv: use following format to run the crawler 
+        scrapy crawl postDF  
+        '''
+    
         page= response.url.split('/')[-1]
         filename = 'posts-%s.csv'%page
         df = pd.DataFrame(columns=['Title','Date','Author', 'Link', 'Content'])
 
         for post in response.css('div.post-item'):
             postheader = post.css('.post-header')
-            posttitle = postheader.css('h2 a::text').get()
             postbyline = postheader.css('.byline')
+
+            posttitle = postheader.css('h2 a::text').get()
             postdate = postbyline.css('.date a::text').get()
             postauthor = postbyline.css('.author a::text').get()
             postContent = post.css('.post-content p ::text').get()
@@ -27,26 +59,5 @@ class PostSpider(scrapy.Spider):
 
             postdata = dict(Title = posttitle, Date = postdate, Author = postauthor, Link = postLink, Content = postContent)
             df = df.append(postdata, ignore_index=True)
-
-       
         df.to_csv(filename)
         
-    # '''  parse the data with xpath selectors  '''
-
-    # def parse(self, response):
-    #     page= response.url.split('/')[-1]
-    #     filename = 'posts-%s.txt'%page
-
-    #     with open(filename, "a") as myfile:
-    #         for post in response.css('div.post-item'):
-    #             postheader = post.css('.post-header')
-    #             posttitle = postheader.css('h2 a::text').get()
-    #             postbyline = postheader.css('.byline')
-    #             postdate = postbyline.css('.date a::text').get()
-    #             postauthor = postbyline.css('.author a::text').get()
-    #             postContent = post.css('.post-content p ::text').get()
-    #             postLink = postheader.css('h2 a::attr(href)').get()
-
-    #             data = dict(title =posttitle ,date=postdate, author=postauthor, link= postLink, postContent=postContent)
-    #             myfile.write("%s\n" % data)
-            
